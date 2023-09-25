04f6e879770a60f62992d37be16ba20063846dc5
Joao Falcao <joaorpalharesfalcao@gmail.com>
2023-09-25 10:52:22 +0100

    ficha1 ex2 done

--- ficha1/ex2.cpp
+++ ficha1/ex2.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <set>
 #include <list>
-#include <iterator>
 #include <iterator>
 #include <fstream>
 using namespace std;
@@ -10,8 +11,7 @@ class listFeatures{
     list<string> lista;
 
 public:
-    void initializeFromFile(string filename)
-    {
+    void initializeFromFile(string filename){
         fstream file(filename, ios::in);
 
         if (!file.is_open())
@@ -24,30 +24,98 @@ public:
         file.close();
     }
 
-    size_t Size(){ return lista.size(); }
+    size_t Size() { return lista.size(); }
 
     void showList(){
         list<string>::iterator it;
-        for(it = lista.begin(); it != lista.end(); ++it) cout << *it << "--";
+        for (it = lista.begin(); it != lista.end(); ++it)
+            cout << *it << "--";
         cout << "nullptr\n";
     }
 
     void showListReversed(){
-        list<string>::iterator it;
+        list<string>::reverse_iterator it;
         cout << "nullptr";
-        for(it = prev(lista.end()); it != prev(lista.begin()); --it) cout << "--" << *it;
+        for (it = lista.rbegin(); it != lista.rend(); ++it)
+            cout << "--" << *it;
         cout << '\n';
     }
 
+    void deleteNodeByValue(string word) { lista.remove(word); }
+
+    void deleteNodeByIndex(int index){
+        if (index < 0 || index >= lista.size())
+            return;
+
+        list<string>::iterator it;
+        it = lista.begin();
+        advance(it, index);
+        lista.erase(it);
+    }
+
+    bool existRepetition(){
+        set<string> seen;
+        list<string>::iterator it;
+        for (it = lista.begin(); it != lista.end(); ++it)
+            if(seen.find(*it) != seen.end()) return false;
+        return true;
+    }
+
+    int countWordsByLength(int maxLength_exclusive){
+        int count = 0;
+
+        list<string>::iterator it;
+        for (it = lista.begin(); it != lista.end(); ++it)
+            if((*it).length() < maxLength_exclusive) count++;
+        
+        return count;
+    }
+
+    void upperCaseAll(){
+        list<string>::iterator it;
+        for (it = lista.begin(); it != lista.end(); ++it) 
+            transform((*it).begin(), (*it).end(), (*it).begin(), ::toupper);
+    }
+
+    void writeWordsToFileByLength(string filename, int minLength_inclusive){
+        fstream file(filename, ios::out);
+
+        if(!file.is_open()) return;
+
+
+        list<string>::iterator it;
+        for (it = lista.begin(); it != lista.end(); ++it)
+            if((*it).length() >= minLength_inclusive) file << *it << '\n';
+        
+
+        file.close();
+    }
 };
 
-int main(){
+int main()
+{
 
     listFeatures lista;
     lista.initializeFromFile("./inputList.txt");
     cout << lista.Size() << endl;
     lista.showList();
     lista.showListReversed();
+
+    /* cout << "\tdeleting" << endl;
+    lista.deleteNodeByValue("eu");
+    lista.showList();
+
+    lista.deleteNodeByIndex(0);
+    lista.showList(); */
+ 
+    cout << "repetition: " << lista.existRepetition() << endl;
+
+    cout << "Count words max length: " << lista.countWordsByLength(5) << endl;
+
+    lista.upperCaseAll();
+    lista.showList();
+
+    lista.writeWordsToFileByLength("./outputList.txt", 1);
 
     return 0;
 }
